f3118fae01aa3352cabae73548c3c9700f4d710a
Cesia-PMartinez <yuniseu977@gmail.com>
2025-03-13 08:49:50 -0600

    Addmain2

---
+++ Clase 13 de marzo/main2.js
@@ -0,0 +1,13 @@
+function myfunction (){
+console.log("Tarea 1");
+
+setTimeout( _=> {console.log ("Tarea 2"), 5000}); //imprime despues de 5 segundos
+console.log("Tarea 3");
+console.log("Tarea 4");
+
+
+}
+
+console.log("Tarea fuera 1");
+myfunction();
+console.log("Tarea fuera 2");
