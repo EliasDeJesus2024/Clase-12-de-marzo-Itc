7d62f9eedda6b2e515bad443a897a3abf9397c33
Cesia-PMartinez <yuniseu977@gmail.com>
2025-03-13 10:41:24 -0600

    MostroData

--- FETCH/main.js
+++ FETCH/main.js
@@ -1,15 +1,3 @@
-async function getData(){
-    const url = "https://rickandmortyapi.com/api/character";
-    try{
-        const data = await fetch(url);
-        if(!data.ok){
-            throw new Error("El error es:, data.status")
-        }
- const dataJson = await data.json();
-        console.log(dataJson)
-    }catch (error){
-        console.log(error.message);
-    }
-}
- 
-getData();
+import showData from "./showData.js";
+
+showData();
